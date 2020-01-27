afdd55a559a543019207935e9fa229b4f2175cd8
lukaa12 <l.wolanin@stud.elka.pw.edu.pl>
2020-01-27 01:56:23 +0100

    Now tree in forest can get more than one attribut

--- RandomForest/src/Forest.java
+++ RandomForest/src/Forest.java
@@ -1,9 +1,22 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.concurrent.ThreadLocalRandom;
+
+class WeakModel{
+    DecisionTree tree;
+    ArrayList<Integer> attribs;
+    double mse;
+
+    WeakModel() {
+        tree = null;
+        attribs = new ArrayList<>();
+        mse = 0.0;
+    }
+}
 
 public class Forest {
-    private ArrayList<DecisionTree> ensemble;
+    private ArrayList<WeakModel> ensemble;
     private ArrayList<Integer> attribs;
 
     public void trainForest(ArrayList<Sample> training){
@@ -16,26 +29,39 @@ public class Forest {
         for(int x = 0; x < training.get(0).getAttributes().size(); ++x)
             attribs.add(x);
         Collections.shuffle(attribs);
+
         for(int i = 0; i < size; ++i){
-            Collections.shuffle(training);
+            WeakModel model = new WeakModel();
+
+            Collections.shuffle(attribs);
+            for(int j = 0; j < Math.sqrt(attribs.size()); ++j)
+                model.attribs.add(attribs.get(j));
+
             ArrayList<Sample> set = new ArrayList<>();
-            for(int j = 0; j < training.size()/size; ++j){
+            for(int j = 0; j < size; ++j){
                 ArrayList<Double> attr = new ArrayList<>();
-                attr.add(training.get(j).getAttributes().get(i));
-                Sample tmp = new Sample(attr,training.get(j).getResult());
-                set.add(tmp);
+                Sample tmp = training.get(ThreadLocalRandom.current().nextInt(0, training.size()));
+                for(Integer atr : model.attribs)
+                    attr.add(tmp.getAttributes().get(atr));
+                set.add(new Sample(attr,tmp.getResult()));
             }
-            DecisionTree tree = new DecisionTree(set,6);
-            ensemble.add(tree);
+
+            model.tree = new DecisionTree(set,3);
+            ensemble.add(model);
         }
     }
 
     public double predict(Sample toPredict) {
         ArrayList<Double> predictions = new ArrayList<>();
-        for(int i = 0; i < ensemble.size(); ++i){
-            Sample prepared = new Sample(new ArrayList<>(Arrays.asList(toPredict.getAttributes().get(i))),toPredict.getResult());
-            predictions.add(ensemble.get(i).getResultForSample(prepared));
+
+        for(WeakModel model : ensemble){
+            ArrayList<Double> attr = new ArrayList<>();
+            for(Integer atr : model.attribs)
+                attr.add(toPredict.getAttributes().get(atr));
+            Sample prepared = new Sample(attr, toPredict.getResult());
+            predictions.add(model.tree.getResultForSample(prepared));
         }
+
         double pred = 0.0;
         for(Double p : predictions){
             pred +=p;
